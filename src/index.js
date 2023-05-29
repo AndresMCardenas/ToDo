658d05f353327e340f0a15da00828bbe1d6aeca0
AndresCooprofesores <andrescar18@gmail.com>
2023-05-29 15:34:22 -0500

    se configura para pasar a produccion

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import app from './app';//aplicaion de express
 import {Server as WebsocketServer} from 'socket.io';
 import http from 'http';
 import sockets from './sockets';// se importa socket io para realizar la escucha activa desde servidor y cliente
+import { PORT } from './config';
 
 import { connectDB } from './db';// se importa la coneccion a la BD
 
@@ -9,8 +10,8 @@ import { connectDB } from './db';// se importa la coneccion a la BD
 connectDB();
 
 const server = http.createServer(app);//se envia la app de expres atravez de creatserver y retorna un servidor
-const httpServer = server.listen(3000);//se hace escuchar al servidor un puerto
-console.log('Server is running on port 3000');
+const httpServer = server.listen(PORT);//se hace escuchar al servidor un puerto
+console.log('Server is running on port', PORT);
 
 const io = new WebsocketServer(httpServer);//se le pasa a websocket el servidor para que lo reconosca 
 sockets(io);
